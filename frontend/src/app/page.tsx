3efbe47d12a73e97cfb1fbbaa595793cc167d289
Andrew Lin <bork.andrew.ryan@gmail.com>
2025-02-18 19:12:09 -0800

    Added prose mirror

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import InteractiveTextInput from "@/components/interactive-text-input/interactive-text-input";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <div className={styles["text-input"]} contentEditable>
-          </div>
+          <InteractiveTextInput/>
       </main>
     </div>
   );
---
+++ frontend/src/components/interactive-text-input/interactive-text-input.tsx
@@ -0,0 +1,70 @@
+"use client"
+
+import styles from "./interactive-text-input.module.css";
+
+import { EditorState, Plugin } from "prosemirror-state";
+import { schema } from "prosemirror-schema-basic";
+import { ProseMirror, useEditorState } from "@nytimes/react-prosemirror";
+import { useState } from "react";
+import { Decoration, DecorationSet } from "prosemirror-view";
+
+let specklePlugin = new Plugin({
+    state: {
+      init(_, {doc}) {
+        let speckles = []
+        for (let pos = 1; pos < doc.content.size; pos += 4)
+          speckles.push(Decoration.inline(pos - 1, pos, {style: "background: yellow"}))
+        return DecorationSet.create(doc, speckles)
+      },
+      apply(tr, set) { return set.map(tr.mapping, tr.doc) }
+    },
+    props: {
+      decorations(state) { return specklePlugin.getState(state) }
+    }
+  });
+  
+
+let purplePlugin = new Plugin({
+    props: {
+        decorations(state) {
+            // console.log(state.doc.textContent)
+            const a = [
+                "color: red",
+                "color: green",
+                "color: blue",
+                "color: purple"
+            ]
+            let x= 0;
+            // state.doc.textContent.split(" ").map((s,i) => {
+            //     let y = x;
+            //     x+=s.length+1;
+            //     return Decoration.inline(y, s.length, {style: a[i]})
+            // });
+
+            return DecorationSet.create(state.doc, 
+                state.doc.textContent.split(" ").map((s,i) => {
+                    let y = x;
+                    x+=s.length+1;
+                    return Decoration.inline(y, y+s.length+3, {style: a[i % a.length]}, {
+                        inclusiveEnd: true
+                    })
+                })
+            )
+        }
+    }
+})
+  
+
+const defaultState = EditorState.create({ schema, plugins: [ purplePlugin ] });
+export default function InteractiveTextInput() {
+    const [mount, setMount] = useState<HTMLElement | null>(null);
+
+
+    // const editorState = useEditorState();
+
+    return (
+        <ProseMirror mount={mount} defaultState={defaultState}>
+          <div ref={setMount} className={styles["input-area"]}/>
+        </ProseMirror>
+      );
+}
