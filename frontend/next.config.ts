ed4aef718b34cf4c93712630c86c8b720b742a46
Andrew Lin <bork.andrew.ryan@gmail.com>
2025-04-14 19:58:51 -0700

    Parameterized DICTIONARY_URL

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,27 +1,29 @@
 import type { NextConfig } from "next";
 
+const DICTIONARY_SERVER = "http://127.0.0.1:8000";
+
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
         source: '/api/tokenize/jp',
-        destination: 'http://127.0.0.1:8000/tokenize/jp',
+        destination: `${DICTIONARY_SERVER}/tokenize/jp`,
       },
       {
         source: '/api/tokenize/cn',
-        destination: 'http://127.0.0.1:8000/tokenize/cn',
+        destination: `${DICTIONARY_SERVER}/tokenize/cn`,
       },
       {
         source: '/api/term/jp/:term',
-        destination: 'http://127.0.0.1:8001/term/jp/:term',
+        destination: `${DICTIONARY_SERVER}/term/jp/:term`,
       },
       {
         source: '/api/kanji/jp/:term',
-        destination: 'http://127.0.0.1:8000/kanji/jp/:term',
+        destination: `${DICTIONARY_SERVER}/kanji/jp/:term`,
       },
       {
         source: '/api/image/ocr',
-        destination: 'http://127.0.0.1:8000/image/ocr',
+        destination: `${DICTIONARY_SERVER}/image/ocr`,
 
       }
     ]
