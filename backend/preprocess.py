adc17b552abdedca2c0f04fceecc776d30538260
bork <bork.andrew.ryan@gmail.com>
2025-04-26 20:36:13 -0700

    Updated open to utf8 in preprocess.py

--- backend/preprocess.py
+++ backend/preprocess.py
@@ -4,11 +4,11 @@ if __name__ == "__main__":
     c_dict = CDict.CDict("./data/cedict_ts.txt")
 
     jieba_dict_entries = []
-    with open("./data/dict.txt.big", "r") as file:
+    with open("./data/dict.txt.big", "r", encoding="utf8") as file:
         jieba_dict_entries = [ i.split(" ") for i in file.readlines()[:-1]]
     # print(jieba_dict_entries)
     jieba_dict_entries = [ i for i in jieba_dict_entries if i[0] in c_dict.index] 
     # print(jieba_dict_entries)
-    with open("./data/dict.txt.reduced", "w") as file:
+    with open("./data/dict.txt.reduced", "w", encoding="utf8") as file:
         file.write("".join([ " ".join(i) for i in jieba_dict_entries]))
         
